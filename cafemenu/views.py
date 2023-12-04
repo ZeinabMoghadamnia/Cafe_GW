c80b59b608b9a7d407dc802f1a4b8f57b00e3579
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-04 14:40:21 +0330

    [Cafe_GW][#1]|simple views for checking the main page

--- cafemenu/views.py
+++ cafemenu/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 # Create your views here.
+def home_page(request):
+    return render(request, 'cafemenu/main_page.html')
