be71f3113fbe741478a0c1a9f359595e0cde615a
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-06 20:52:06 +0330

    [Cafe_GW][#1]|set middleware in setting

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cafemenu.middleware.NoCachingMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
