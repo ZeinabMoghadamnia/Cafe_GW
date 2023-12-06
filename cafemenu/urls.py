60831dea7f44056609518bdef47aa43093876558
Zeinab-Mo <zeinab.moghadamniaa@gmail.com>
2023-12-06 15:05:34 +0330

    [Cafe_GW][#7]|first update

--- cafemenu/urls.py
+++ cafemenu/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import HomePage, Contact
 
 urlpatterns = [
-    path('', views.home_page),
-    path('contact/', views.contact),
+    path('', HomePage.as_view(), name='home'),
+    path('contact/', Contact.as_view(), name='contact'),
 ]
--- cafemenu/views.py
+++ cafemenu/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.views.generic import DetailView, TemplateView
 
 
 # Create your views here.
-def home_page(request):
-    return render(request, 'cafemenu/main_page.html')
 
-def contact(request):
-    return render(request, 'cafemenu/contact.html')
+class HomePage(TemplateView):
+    template_name = 'cafemenu/main_page.html'
+    
+class Contact(TemplateView):
+    template_name = 'cafemenu/contact.html'
+
+# def home_page(request):
+#     return render(request, 'cafemenu/main_page.html')
+
+# def contact(request):
+#     return render(request, 'cafemenu/contact.html')
