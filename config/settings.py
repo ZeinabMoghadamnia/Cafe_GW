bc53d60459c2dcb6fb3c3eed12574ec4fa695787
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-08 16:54:28 +0330

    [Cafe_GW][#1]|AUTH_USER_MODEL added to setting

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,9 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomerUser'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
