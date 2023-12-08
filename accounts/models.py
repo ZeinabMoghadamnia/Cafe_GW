68458949b41e03d47345e506052a22acae27991f
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-08 17:44:24 +0330

    [Cafe_GW][#1]|customuser class edited

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomerUser(AbstractUser):
     birth_day = models.DateField(null=True, blank=True)
+    phone_number = models.CharField(max_length=20)
+    user_type = models.CharField(max_length=20)
+    city = models.CharField(max_length=30, null=True, blank=True)
+    address = models.CharField(max_length=150, null=True, blank=True)
+    image = models.ImageField()
