878d0a2e775cc8b2190ec104e6bf7cf680ea768b
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-08 16:48:32 +0330

    [Cafe_GW][#1]|admin file created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomerUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomerUser
+    list_display = ['username', 'email', 'birth_day', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        ('jafar', {'fields': ('birth_day',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('birth_day',)}),
+    )
+
+
+admin.site.register(CustomerUser, CustomUserAdmin)
