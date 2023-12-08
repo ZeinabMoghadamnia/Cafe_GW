ba9cebceba7693cabe003a4612dd33760d42ae12
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-08 17:01:02 +0330

    [Cafe_GW][#1]|category model has been created

--- cafemenu/models.py
+++ cafemenu/models.py
@@ -4,3 +4,11 @@ from django.utils.text import slugify
 from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.category_name()
+
