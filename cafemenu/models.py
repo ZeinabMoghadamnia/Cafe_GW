5fd423f74d7ff7b506661c5c37342a613eafad15
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-08 18:34:36 +0330

    [Cafe_GW][#1]|shopping cart commentet in model

--- cafemenu/models.py
+++ cafemenu/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
 
 
 class MenuItem(models.Model):
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Category') # ask abolfazl for related name and first item
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='menuitem') # ask abolfazl for related name and first item
     item_name = models.CharField(max_length=150)
     price = models.IntegerField()
     mojoudi = models.IntegerField()
@@ -27,13 +27,12 @@ class MenuItem(models.Model):
 
 class Image(models.Model):
     path = models.CharField(max_length=255)
-    item_id = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='MenuItem')    # ask abolfazl
-    # user_id     # ask abolfazl
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='image')    # ask abolfazl
     
     
-class ShoppingCart(models.Model):
-    item_id = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='MenuItem')    # ask abolfazl
-    quantity = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True) # ask abolfazl
-    updated_at = models.DateTimeField(auto_now=True)    # ask abolfazl
-    total_price = models.IntegerField()
+# class ShoppingCart(models.Model):
+#     item_id = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='shoppingcart')    # ask abolfazl
+#     quantity = models.IntegerField()
+#     created_at = models.DateTimeField(auto_now_add=True) # ask abolfazl
+#     updated_at = models.DateTimeField(auto_now=True)    # ask abolfazl
+#     total_price = models.IntegerField()
