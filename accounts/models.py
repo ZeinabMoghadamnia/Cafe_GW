d8d346a64e719c5d133dfbbdfc326a42b620588b
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-08 19:18:21 +0330

    [Cafe_GW][#1]|regex added for phone_number

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.core.validators import RegexValidator
 
 class CustomerUser(AbstractUser):
     CUSTOMERUSER_EMPLOYEE = 'e'
@@ -12,7 +12,8 @@ class CustomerUser(AbstractUser):
         (CUSTOMERUSER_MANAGER,'manager')
     )
     birth_day = models.DateField(null=True, blank=True)
-    phone_number = models.CharField(max_length=20)
+    mobile_regex = RegexValidator(regex='^(\+98|0)?9\d{9}$', message="Phone number must be entered in the format: '+989199999933'.")
+    phone_number = models.CharField(validators=[mobile_regex], max_length=20)
     user_type = models.CharField(max_length=1,choices=CUSTOMERUSER_STATUS, default=CUSTOMERUSER_CUSTOMER)
     city = models.CharField(max_length=30, null=True, blank=True)
     address = models.CharField(max_length=150, null=True, blank=True)
