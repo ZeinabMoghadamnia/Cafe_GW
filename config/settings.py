9e7db3cac12b51ddb77011acbdfb9edf72d918f9
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-09 23:13:31 +0330

    [Cafe_GW][#1]|setting passworf changed

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': "Cafe_GW",
         'USER': "postgres",
-        'PASSWORD': "sq8151ze",
+        'PASSWORD': "0020@002062",
         'HOST': "localhost",
         'PORT': "5432",
     }
