9ffb139a9f66dd7dc1352727e9c042180597cb02
meysam tajik <meysamtajik1376@gmail.com>
2023-12-12 19:15:48 +0330

    [Cafe_GW][#6]| create admin dashbord

--- dashbord/admin.py
+++ dashbord/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Post
+from import_export import resources
 # Register your models here.
+class PostResourse(resources.ModelResource):
+    class Meta:
+        model =Post
+        fields = ("id","title", "description" ,"is_active" , "created")
+        export_order = ("id","description" ,"is_active" ,"title", "created")
+
+admin.site.register(Post)
