8a731678745f4ad860e2314799805334e4dd2bca
Zeinab-Mo <zeinab.moghadamniaa@gmail.com>
2023-12-19 18:24:01 +0330

    [Cafe_GW][#7]|chart added

--- config/urls.py
+++ config/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('', include('cafemenu.urls')),
+    path('report', include('data_chart.urls')),
     # path('dashbord', include('dashbord.urls', namespace="dashbord")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ data_chart/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DataChartConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'data_chart'
