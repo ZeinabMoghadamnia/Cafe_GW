54f2fb2e57c2349913d1be6dd7dc656af07a0f6a
Zeinab-Mo <zeinab.moghadamniaa@gmail.com>
2023-12-19 20:03:34 +0330

    [Cafe_GW][#7]|urls updated

--- config/settings.py
+++ config/settings.py
@@ -149,8 +149,8 @@ STATICFILES_DIRS = [
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomerUser'
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'cafemenu:home'
+LOGOUT_REDIRECT_URL = 'cafemenu:home'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
